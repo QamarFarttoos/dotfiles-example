63a63bd74c555cb24e74bd7e21e862c6a251f9ba
Qamar <qamarfarttoos@u.boisestat.edu>
2026-03-12 13:27:22 -0600

    Fixed bash_settings syntax error

--- bash_settings.sh
+++ bash_settings.sh
@@ -7,18 +7,15 @@ append_path() {
     esac
 }
 
-# Add directories to PATH
 append_path "$HOME/bin"
 append_path "$HOME/.local/bin"
 
-# Set the prompt for both ZSH and BASH
 if [ -n "$ZSH_VERSION" ]; then
     precmd () { __git_ps1 "%n" ":%~$ " "|%s"; }
 elif [ -n "$BASH_VERSION" ]; then
     PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
 fi
 
-# Turn on git-prompt settings
 GIT_PS1_SHOWCOLORHINTS=true
 GIT_PS1_SHOWUPSTREAM=false
 GIT_PS1_DESCRIBE_STYLE=branch
@@ -26,6 +23,5 @@ GIT_PS1_SHOWDIRTYSTATE=false
 GIT_PS1_SHOWSTASHSTATE=false
 GIT_PS1_SHOWUNTRACKEDFILES=true
 
-# Aliases
 alias ll='ls -l'
 alias la='ls -l -a'
